bc426a8f5f62c40e9278800a62ca94ae4779ec78
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2026-03-16 19:45:43 -0300

    DI-678. feat: title e meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>MobFácil</title>
+        <meta name="description" content="MobFácil - Onboarding Digital Fácil e Seguro" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
